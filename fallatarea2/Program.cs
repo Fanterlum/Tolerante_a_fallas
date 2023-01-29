e18b67311dc9dfb635759d68f646e576d0d3cb1a
Fanterlum <vicente.gonzalez5380@alumnos.udg.mx>
2023-01-29 16:16:07 -0600

    Add project files.

---
+++ fallatarea2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CheckedUncheckedExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.MaxValue;
+            int b = int.MaxValue;
+            int result;
+
+            try
+            {
+                // Utiliza la cláusula checked para habilitar la revisión de desbordamiento
+                result = checked(a + b);
+                Console.WriteLine("Resultado con la cláusula checked: " + result);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            // Utiliza la cláusula unchecked para deshabilitar la revisión de desbordamiento
+            result = unchecked(a + b);
+            Console.WriteLine("Resultado con la cláusula unchecked: " + result);
+
+            Console.ReadLine();
+        }
+    }
+}
